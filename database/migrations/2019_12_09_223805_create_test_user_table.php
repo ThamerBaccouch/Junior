e50bbaa396f6b4da912da602e70f58ecce934584
Thamer Baccouch <baccouch.thamer@insat.u-carthage.tn>
2019-12-09 23:53:48 +0100

    migrations added

---
+++ database/migrations/2019_12_09_223805_create_test_user_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTestUserTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('test_user', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->bigInteger("id_user");
+            $table->bigInteger("id_test");
+            $table->string("score");
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('test_user');
+    }
+}
